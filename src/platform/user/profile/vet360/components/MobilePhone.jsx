370e983ee4999dd37cac12c3d5e41e1392a574f5
Elwood Gary <kenneth.gary@va.gov>
2019-08-21 15:02:18 -0400

    Saving some work before switching to new computer.

--- src/platform/user/profile/vet360/components/MobilePhone.jsx
+++ src/platform/user/profile/vet360/components/MobilePhone.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
+
 import ReceiveTextMessages from 'vet360/containers/ReceiveTextMessages';
 import PhoneField from './PhoneField';
 import { FIELD_NAMES } from '../constants';
 import { selectVet360Field } from '../selectors';
 
-function MobilePhone({shouldDisplayReceiveTextMessages, isEnrolledInHealthcare}) {
+function MobilePhone({
+  shouldDisplayReceiveTextMessages,
+  isEnrolledInHealthcare,
+}) {
   return (
     <>
       <PhoneField
         title="Mobile phone number"
         fieldName={FIELD_NAMES.MOBILE_PHONE}
       />
-      {shouldDisplayReceiveTextMessages && isEnrolledInHealthcare &&
-        <ReceiveTextMessages />
-      }
+      {shouldDisplayReceiveTextMessages &&
+        isEnrolledInHealthcare && <ReceiveTextMessages />}
     </>
   );
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = state => {
   const data = selectVet360Field(state, FIELD_NAMES.MOBILE_PHONE);
   const shouldDisplayReceiveTextMessages = data;
   // TODO: get the real flag...
@@ -31,6 +33,4 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-)(MobilePhone);
+export default connect(mapStateToProps)(MobilePhone);
--- src/platform/user/profile/vet360/containers/ReceiveTextMessages.jsx
+++ src/platform/user/profile/vet360/containers/ReceiveTextMessages.jsx
@@ -1,65 +1,74 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import isEqual from 'lodash/isEqual';
-import pick from 'lodash/pick';
+
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 import ErrorableCheckbox from '@department-of-veterans-affairs/formation-react/ErrorableCheckbox';
 import { FIELD_NAMES } from '../constants';
 import { selectVet360Field } from '../selectors';
 
-class ReceiveTextMessages extends React.Component {  
-  
-  isSuccessAlertVisible = () => {
+class ReceiveTextMessages extends React.Component {
+  state = { showSuccess: false, checkboxValue: this.props.checked };
+
+  onChange = event => {
+    this.setState({ checkboxValue: event });
+    // start the api call to update isTextPermitted with value of event (true/false)
+    // make checkbox disabled until api call comes back
+    // when api call comes back as a success then re-enable checkbox and make success message visable
+    // TODO: Figure out where/when to make the success alert invisible again after that
+  };
+
+  isSuccessAlertVisible = () =>
     // TODO: Put logic here to control success alert box.
-    return true;
-  }
-  
+    this.state.showSuccess;
+
   render() {
-    // TODO: Do not show this if user is not enrolled in health care also.
-    if (!this.props.isTextable) return <div />;
+    if (!this.props.isTextable || !this.props.isEnrolledInHealthcare)
+      return <div />;
     return (
       <div className="receive-text-messages">
         <div className="form-checkbox-buttons">
           <ErrorableCheckbox
             name="isTextPermitted"
-            checked={this.props.checked}
-            label={<span>Receive text messages (SMS) for VA health care appointment reminders.</span>}
+            checked={this.state.checkboxValue}
+            label={
+              <span>
+                Receive text messages (SMS) for VA health care appointment
+                reminders.
+              </span>
+            }
+            onValueChange={this.onChange}
           />
-        <AlertBox
-          isVisible={this.isSuccessAlertVisible()}
-          content={<p>Your preference has been saved.</p>}
-          status="success"
-          backgroundOnly />
+          <AlertBox
+            isVisible={this.isSuccessAlertVisible()}
+            content={<p>Your preference has been saved.</p>}
+            status="success"
+            backgroundOnly
+          />
         </div>
       </div>
     );
   }
 }
 
-export function mapStateToProps(state, ownProps) {
-  
-  const { convertNextValueToCleanData } = ownProps;
-  
+export function mapStateToProps(state) {
   const mobilePhone = selectVet360Field(state, FIELD_NAMES.MOBILE_PHONE);
-  
+
   const isTextable = mobilePhone.isTextable;
-  
-  const isTextPermitted = mobilePhone.isTextPermitted
+
+  const isTextPermitted = mobilePhone.isTextPermitted;
 
   const checked = isTextable && isTextPermitted;
- 
+
+  // TODO: get the real flag...
+  const isEnrolledInHealthcare = true;
+
   return {
     mobilePhone,
     isTextable,
     isTextPermitted,
     checked,
+    isEnrolledInHealthcare,
   };
-  
 }
 
-export default connect(
-  mapStateToProps,
-  null,
-)(ReceiveTextMessages);
-
-export { ReceiveTextMessages };
+export default connect(mapStateToProps)(ReceiveTextMessages);
